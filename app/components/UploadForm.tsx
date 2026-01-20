02bd76499cab4d895d15d46a797e5c6f2bd3394b
C Stavridis <castavridis@gmail.com>
2026-01-20 15:35:50 -0600

    Tidy UI for Impossible Day demos

--- app/components/UploadForm.tsx
+++ app/components/UploadForm.tsx
@@ -24,56 +24,56 @@ export default function UploadForm() {
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label>
-          Paste a valid URL below.<br/>
-          <input placeholder="https://" className="py-1.5 px-3 border-2 border-zinc-200 rounded-md" {...register("imgUrl")} />
-        </label>
-        <br/><div className="border-2 border-zinc-200 rounded-md my-4 p-12">
-          <p className="font-bold">Image preview</p>
-          {
-            imgSrc 
-              ? <img src={imgSrc} />
-              : 'Waiting for an image...'
-          }
+        <div className="flex gap-4">
+          <div className="w-full border-2 border-zinc-200 rounded-md p-12">
+            <p className="font-bold">Image preview</p>
+            <label className="block">
+              Paste a valid URL below.<br/>
+              <input placeholder="https://" className="py-1.5 px-3 border-2 border-zinc-200 rounded-md mb-4" {...register("imgUrl")} />
+              { imgSrc && <img className="block mb-4" src={imgSrc} />}
+              <ul className="pl-4 list-disc">
+                <li>https://upload.wikimedia.org/wikipedia/commons/c/cc/Reptilian_Fabric.jpg</li>
+                <li>https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.studenti.stbm.it%2Fimages%2F2016%2F11%2F07%2Fcomposizione-su-bianco---kandinsky-orig.jpeg&f=1&nofb=1&ipt=5c47160e938dc12af958b35aa0daf169d58ab7acbe0a06bdcd85fa8811f3d7f5</li>
+              </ul>
+            </label>
+          </div>
+          <div className="w-full p-12 border-2 rounded-md border-gray-200">
+              <div>
+                <p className="font-bold">Your assessment</p>
+                <label className="block mb-2">
+                  Sincerity <input
+                  placeholder="1-5" className="py-1.5 px-3 border-2 border-zinc-200 rounded-md" />
+                </label>
+                <label className="block mb-2">
+                  Excitement <input
+                  placeholder="1-5" className="py-1.5 px-3 border-2 border-zinc-200 rounded-md" />
+                </label>
+                <label className="block mb-2">
+                  Competence <input
+                  placeholder="1-5" className="py-1.5 px-3 border-2 border-zinc-200 rounded-md" />
+                </label>
+                <label className="block mb-2">
+                  Sophistication <input
+                  placeholder="1-5" className="py-1.5 px-3 border-2 border-zinc-200 rounded-md" />
+                </label>
+                <label className="block mb-2">
+                  Ruggedness <input
+                  placeholder="1-5" className="py-1.5 px-3 border-2 border-zinc-200 rounded-md" />
+                </label>
+              </div>
+              <div className="border-t-2 border-zinc-200 mt-8 pt-8">
+                <p className="font-bold">Qwen3 VL 235B A22B Instruct Assessment</p>
+                <button 
+                disabled={!imgSrc}
+                className="rounded-md bg-green-300 disabled:border-red-300 disabled:border-2 disabled:bg-red-200 py-1.5 px-3">
+                  {!imgSrc ? "Waiting for image..." : "Analyze Image"}
+                </button>
+                { isLoading && <div>Analyzing...</div>}
+                { result && <pre className="mt-2 border-2 border-zinc-200 p-4 text-zinc-700">{result.message.content}</pre> }
+              </div>
+          </div>
         </div>
-        <br/>
-        <button 
-        disabled={!imgSrc}
-        className="rounded-md bg-green-300 disabled:border-red-300 disabled:border-2 disabled:bg-red-200 py-1.5 px-3">
-          {!imgSrc ? "Waiting for image..." : "Analyze Image"}
-        </button>
       </form>
-
-      <div className="my-4 p-12 border-2 rounded-md border-gray-200">
-          <p className="font-bold">Qwen3 VL 235B A22B Instruct Assessment</p>
-          {!isLoading && !result && <div>Waiting for assessment.</div>}
-          { isLoading && <div>Analyzing...</div>}
-          { result && <pre>{result.message.content}</pre> }
-      </div>
-
-      <div>
-        <p>Your assessment</p>
-        <label className="block">
-          Sincerity <input
-          placeholder="1-5" className="py-1.5 px-3 border-2 border-zinc-200 rounded-md" />
-        </label>
-        <label className="block">
-          Excitement <input
-          placeholder="1-5" className="py-1.5 px-3 border-2 border-zinc-200 rounded-md" />
-        </label>
-        <label className="block">
-          Competence <input
-          placeholder="1-5" className="py-1.5 px-3 border-2 border-zinc-200 rounded-md" />
-        </label>
-        <label className="block">
-          Sophistication <input
-          placeholder="1-5" className="py-1.5 px-3 border-2 border-zinc-200 rounded-md" />
-        </label>
-        <label className="block">
-          Ruggedness <input
-          placeholder="1-5" className="py-1.5 px-3 border-2 border-zinc-200 rounded-md" />
-        </label>
-      </div>
 
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import UploadForm from './components/UploadForm';
 export default function Page() {
   return (
     <div className="container py-12 mx-auto">
-      <h1 className="text-3xl font-bold font-mono">Hello this is the vv landing page.</h1>
+      <h1 className="text-3xl font-bold font-mono mb-4">vv. brand assessment</h1>
       <UploadForm />
     </div>
   )
