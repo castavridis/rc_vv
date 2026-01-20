de301ee4819e4b262cce5d9026c2caf3a9e7b625
C Stavridis <castavridis@gmail.com>
2026-01-20 11:35:23 -0600

    Add initial support for OpenRouter

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+import { OpenRouter } from '@openrouter/sdk'
+
+const openRouter = new OpenRouter({
+  apiKey: process.env.OPENROUTER_API_KEY,
+})
+
+const completion = await openRouter.chat.send({
+  model: 'minimax/minimax-m1',
+  messages: [
+    {
+      role: 'user',
+      content: 'What is the meaning of life?',
+    }
+  ],
+  stream: false,
+})
+
 export default function Page() {
-	return <h1>Hello!</h1>
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <p>Here is OpenRouter's response:</p>
+      <p>{ JSON.stringify(completion.choices[0]) }</p>
+    </div>
+  )
 }
